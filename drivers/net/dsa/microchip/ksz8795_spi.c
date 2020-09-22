03324eb1cc88799259e8d1e3f8d1404716c568a1
Guido Classen <guido.classen@swarco.de>
2020-09-22 16:15:34 +0200

    Output debug messages

--- drivers/net/dsa/microchip/ksz8795_spi.c
+++ drivers/net/dsa/microchip/ksz8795_spi.c
@@ -22,17 +22,52 @@
 
 KSZ_REGMAP_TABLE(ksz8795, 16, SPI_ADDR_SHIFT,
 		 SPI_TURNAROUND_SHIFT, SPI_ADDR_ALIGN);
+#define SPI_CMD_READ	0x60
+#define SPI_CMD_WRITE	0x40
+
+static inline int ksz_spi_read_raw(struct ksz_device *kszd, u8 reg, u8 *data,
+				   unsigned int len)
+{
+	struct spi_device *spi = kszd->priv;
+	u8 txbuf[2];
+
+	/* Command (RD=011, 4 dummy bits, 8 address bits, 1 turnaround bit */
+	txbuf[0] = SPI_CMD_READ | (reg >> 7);
+	txbuf[1] = reg << 1;
+
+	return spi_write_then_read(spi, txbuf, 2, data, len);
+}
+static int ksz_spi_read8(struct ksz_device *kszd, u8 reg, u8 *val)
+{
+	int ret;
+	
+	mutex_lock(&kszd->regmap_mutex);
+	ret = ksz_spi_read_raw(kszd, reg, val, 1);
+	mutex_unlock(&kszd->regmap_mutex);
+	
+	return ret;
+}
 
 static int ksz8795_spi_probe(struct spi_device *spi)
 {
 	struct regmap_config rc;
 	struct ksz_device *dev;
 	int i, ret;
+	printk(KERN_INFO "-- ksz8795_spi_probe() #1\n");
 
 	dev = ksz_switch_alloc(&spi->dev, spi);
+	printk(KERN_INFO "-- ksz8795_spi_probe() #2\n");
 	if (!dev)
 		return -ENOMEM;
+	{
+		u8 val = 0;
 
+#define REG_CHIP_ID0			0x00
+
+#define FAMILY_ID			0x87
+		printk(KERN_INFO "-- read_spi_id() %d %02x\n", ksz_spi_read8(dev, REG_CHIP_ID0, &val), val);;
+	}
+		
 	for (i = 0; i < ARRAY_SIZE(ksz8795_regmap_config); i++) {
 		rc = ksz8795_regmap_config[i];
 		rc.lock_arg = &dev->regmap_mutex;
@@ -45,15 +80,34 @@ static int ksz8795_spi_probe(struct spi_device *spi)
 			return ret;
 		}
 	}
+	printk(KERN_INFO "-- ksz8795_spi_probe() #3\n");
+	{
+		u8 val = 0;
+
+		printk(KERN_INFO "-- read_id() %d %02x\n", ksz_read8(dev, REG_CHIP_ID0, &val), val);;
+	}
+		
 
 	if (spi->dev.platform_data)
 		dev->pdata = spi->dev.platform_data;
 
 	ret = ksz8795_switch_register(dev);
+	printk(KERN_INFO "-- ksz8795_spi_probe() #4\n");
+	{
+		u8 val = 0;
+
+		printk(KERN_INFO "-- read_spi_id#2() %d %02x\n", ksz_spi_read8(dev, REG_CHIP_ID0, &val), val);;
+	}
+	{
+		u8 val = 0;
+
+		printk(KERN_INFO "-- read_id()#2 %d %02x\n", ksz_read8(dev, REG_CHIP_ID0, &val), val);;
+	}
 
 	/* Main DSA driver may not be started yet. */
 	if (ret)
 		return ret;
+	printk(KERN_INFO "-- ksz8795_spi_probe() #5\n");
 
 	spi_set_drvdata(spi, dev);
 
@@ -89,6 +143,7 @@ MODULE_DEVICE_TABLE(of, ksz8795_dt_ids);
 static struct spi_driver ksz8795_spi_driver = {
 	.driver = {
 		.name	= "ksz8795-switch",
+		.bus = &spi_bus_type,
 		.owner	= THIS_MODULE,
 		.of_match_table = of_match_ptr(ksz8795_dt_ids),
 	},
